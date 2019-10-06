251985c3f5f11e91b3464495a883bb054752b7b1
nedelko <valentynnedelko@gmail.com>
2019-10-06 22:08:20 +0300

    Add project files.

---
+++ Calculator/Models/CalculatorResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator.Models
+{
+    public class CalculatorResult
+    {
+        public int id { get; set; }
+        public string calculationString { get; set; }
+        public double result { get; set; }
+    }
+}
